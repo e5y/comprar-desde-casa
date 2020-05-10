415037eed3fc3c6ffbd14d80cf4efdbe97c0342d
e5y <ezequiel6arrido+e5y@gmail.com>
2020-05-10 13:10:57 -0300

    Fixed missconfiguration of firebase database url.

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const config = {
     firebase: {
         apiKey: process.env.CDC_FIREBASE_API_KEY,
         authDomain: process.env.CDC_FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.CDC_FIREBASE_DATABSE_URL,
+        databaseURL: process.env.CDC_FIREBASE_DATABASE_URL,
         projectId: process.env.CDC_FIREBASE_PROJECT_ID,
         storageBucket: process.env.CDC_FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.CDC_FIREBASE_MESSAGING_SENDER_ID,
