b567684db2c38e9a48b0e59815a2c1642922930b
e5y <ezequiel6arrido+e5y@gmail.com>
2020-05-22 17:29:18 -0300

    Refactored BusinessProfile.

--- src/Pages/BusinessProfile.svelte
+++ src/Pages/BusinessProfile.svelte
@@ -2,9 +2,10 @@
   import { onMount } from "svelte";
 
   import { db } from "../stores.js";
+
+  import { getPosition } from "../utilities.js";
   import { Business } from "../classes/Business.js";
 
-  import Layout from "../Layout/Layout.svelte";
   import Loader from "../Utility/Loader.svelte";
   import Info from "../Utility/Info.svelte";
 
@@ -13,63 +14,50 @@
 
   export let id;
 
-  let business, position;
-
-  const getPosition = () =>
-    new Promise((res, rej) => {
-      navigator.geolocation.getCurrentPosition(
-        position => res(position),
-        e => rej(e),
-        { enableHighAccuracy: true }
-      );
-    });
+  let position, business;
 
   onMount(async () => {
     try {
       position = await getPosition();
     } catch {
+      position = false;
       return;
     }
-    business = new Business(
-      await $db
-        .collection("approved_businesses")
-        .doc(id)
-        .get()
-    );
+    business = await $db.getApprovedBusiness(id);
   });
 </script>
 
-<Layout>
-  {#if position}
-    {#if business}
-      <BusinessCard {business} />
-      <Map {position} businesses={[business]} type="compact" />
-    {:else}
-      <Loader />
-    {/if}
+{#if position}
+  {#if business}
+    <BusinessCard {business} />
+    <Map {position} businesses={[business]} type="compact" />
   {:else}
-    <Info type="error">
-      ¡Oh no! Parece que no tenés habilitada tu ubicación. Revisá la
-      configuración de tu dispositivo para ver los negocios que estén cerca
-      <b style="margin-top: .5rem;display: inline-block;">
-        Cómo habilitar tu ubicación:
-      </b>
-      <ul class="enable-location">
-        <li>
-          <i class="fab fa-android" />
-          <a
-            target="_blank"
-            href="https://support.google.com/nexus/answer/3467281?hl=es">
-            En Android
-          </a>
-        </li>
-        <li>
-          <i class="fab fa-apple" />
-          <a target="_blank" href="https://support.apple.com/es-lamr/HT207092">
-            En iOS (iPhone/iPad)
-          </a>
-        </li>
-      </ul>
-    </Info>
+    <Loader />
   {/if}
-</Layout>
+{:else if position === false}
+  <Info type="error">
+    ¡Oh no! Parece que no tenés habilitada tu ubicación. Revisá la configuración
+    de tu dispositivo para ver los negocios que estén cerca
+    <b style="margin-top: .5rem;display: inline-block;">
+      Cómo habilitar tu ubicación:
+    </b>
+    <ul class="enable-location">
+      <li>
+        <i class="fab fa-android" />
+        <a
+          target="_blank"
+          href="https://support.google.com/nexus/answer/3467281?hl=es">
+          En Android
+        </a>
+      </li>
+      <li>
+        <i class="fab fa-apple" />
+        <a target="_blank" href="https://support.apple.com/es-lamr/HT207092">
+          En iOS (iPhone/iPad)
+        </a>
+      </li>
+    </ul>
+  </Info>
+{:else}
+  <Loader />
+{/if}
